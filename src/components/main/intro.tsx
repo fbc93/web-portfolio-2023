8f2d10eb5d8ee117a5fdbdeb03e2f33127d3f0e5
yhwa <hfbc93@gmail.com>
2023-06-11 17:17:49 +0900

    Feat: 메인화면 intro 흐르는 텍스트 배경 스타일

--- src/components/main/intro.tsx
+++ src/components/main/intro.tsx
@@ -5,7 +5,17 @@ export default function Intro() {
   return (
     <article className={introStyles.intro}>
       <div className={introStyles.container}>
-        Intro
+        <div className={introStyles.title}>
+          <div className={introStyles.title_container}>
+            <strong>Sub Title</strong>
+            <div>Main Logo</div>
+          </div>
+        </div>
+        <div className={introStyles.description}>
+          intro description
+        </div>
+        <div className={introStyles.scroll_guide}>Scroll Down</div>
+        <div className={introStyles.bg_moving_text}></div>
       </div>
     </article>
   )
