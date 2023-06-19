75dd01ec4362ed4a1a79b14bde361603ee3f0df3
yhwa <hfbc93@gmail.com>
2023-06-19 23:44:47 +0900

    Feat: work detail의 images 레이아웃 컴포넌트 추가 및 반응형 작업

--- src/components/detail/imagesLayout.tsx
+++ src/components/detail/imagesLayout.tsx
@@ -14,8 +14,8 @@ export default function ImagesLayout() {
         </div>
       </div>
       <div className={imagesLayoutStyle.content_container}>
-        <div>content_01</div>
-        <div>content_02</div>
+        <div>해당 업무는 마케팅 담당자가 요청하는 경우가 많아서, 템플릿을 한 곳에서 확인할 수 있는 사이트를 별도로 만들었습니다. 담당자는 기존의 요청과 산출물을 다시 볼 수 있어 활용이 쉬워지고, 개발자는 히스토리를 쉽게 관리할 수 있게 되었습니다.</div>
+        <div>해당 업무는 마케팅 담당자가 요청하는 경우가 많아서, 템플릿을 한 곳에서 확인할 수 있는 사이트를 별도로 만들었습니다. 담당자는 기존의 요청과 산출물을 다시 볼 수 있어 활용이 쉬워지고, 개발자는 히스토리를 쉽게 관리할 수 있게 되었습니다.</div>
       </div>
     </div>
   );
